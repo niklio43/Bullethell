514203900479a6aafb4098d70df26894552c16a2
Niklas Jörgensen <niklio15csgo@hotmail.com>
2022-12-05 15:54:06 +0100

    yes

--- Assets/Scripts/Item/AssultRifle.cs
+++ Assets/Scripts/Item/AssultRifle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BulletHell.Emitters;
 
 [CreateAssetMenu(fileName = "New Ranged Weapon", menuName = "Weapon/Ranged/AssultRifle")]
 public class AssultRifle : Ranged
@@ -8,11 +9,13 @@ public class AssultRifle : Ranged
     [SerializeField] Sprite sprite;
     [SerializeField] int damage;
     [SerializeField] string itemName;
+    [SerializeField] EmitterData emitterData;
 
     void OnEnable()
     {
         Sprite = sprite;
         Damage = damage;
         ItemName = itemName;
+        EmitterData = emitterData;
     }
 }
--- Assets/Scripts/Item/Weapon.cs
+++ Assets/Scripts/Item/Weapon.cs
@@ -1,6 +1,9 @@
+using BulletHell.Emitters;
 public abstract class Weapon : Item
 {
     int _damage;
+    EmitterData _emitterData;
 
     public int Damage { get { return _damage; } set { _damage = value; } }
+    public EmitterData EmitterData { get { return _emitterData; } set { _emitterData = value; } }
 }
