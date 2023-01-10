8f084cc438679af5238eee395fd8a724eb6a7e01
Niklas Jörgensen <niklio15csgo@hotmail.com>
2023-01-10 16:27:36 +0100

    fix error

--- Assets/Scripts/Character/PlayerControls.cs
+++ Assets/Scripts/Character/PlayerControls.cs
@@ -29,10 +29,6 @@ public class PlayerControls : MonoBehaviour
         _inputs.Player.Move.canceled += ctx => _playerStateMachine.Move(ctx);
 
         //UI
-        _inputs.Player.LeftHotBar.performed += ctx => _playerUI.LeftHotBar(ctx);
-        _inputs.Player.RightHotBar.performed += ctx => _playerUI.RightHotBar(ctx);
-        _inputs.Player.BottomHotBar.performed += ctx => _playerUI.BottomHotBar(ctx);
-        _inputs.Player.TopHotBar.performed += ctx => _playerUI.TopHotBar(ctx);
         _inputs.Player.Inventory.performed += ctx => _playerUI.ToggleInventory();
         _inputs.Player.Interact.performed += ctx => playerInteracter.Interact();
 
