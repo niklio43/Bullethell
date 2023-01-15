a121d32f39cdd2ff8fc084bd0e8ebcdb3cb8b697
EricLocher <eriloc1129@skola.goteborg.se>
2023-01-15 23:23:47 +0100

    Some organizing + utilities

---
+++ Assets/Scripts/Systems/Bootstrapper.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class Bootstrapper
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
+}
