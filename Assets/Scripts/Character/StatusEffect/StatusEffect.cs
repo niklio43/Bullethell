3d4d30d296fe310450de9b5e50ab586f00312d6a
EricLocher <eriloc1129@skola.goteborg.se>
2023-02-07 14:31:42 +0100

    Updated statusEffects

--- Assets/Scripts/Character/StatusEffect/StatusEffect.cs
+++ Assets/Scripts/Character/StatusEffect/StatusEffect.cs
@@ -9,91 +9,131 @@ namespace BulletHell.StatusSystem
     [System.Serializable]
     public class StatusEffect
     {
+        [Header("General")]
         public string Name;
         public Sprite Icon;
-        public float TickSpeed = 1;
-        public float Lifetime;
-        public float MaxStacks = 5;
         public DamageInfo DamageInfo;
-        public List<EffectBehaviour> Behaviours = new List<EffectBehaviour>();
-        [HideInInspector] public Character Sender, Reciever;
-        public StatusBehaviour statusBehaviour = StatusBehaviour.None;
 
+        [Header("Stacking Behaviour")]
+        [SerializeField] float maxStacks = 5;
+        [SerializeField] StatusBehaviour _stackingBehaviour = StatusBehaviour.None;
+
+        [Header("Time")]
+        [SerializeField] float tickSpeed = 1;
+        [SerializeField] float lifeTime = 1;
+
+        [Header("Behaviours")]
+        [SerializeField] List<EffectBehaviour> _behaviours = new List<EffectBehaviour>();
         [SerializeField] List<DamageValue> _damageValues = new List<DamageValue>();
-        float _nextTickTime = 0f;
-        float _timer = 0f;
-        float _currentStacks = 1;
+
+        public enum StatusBehaviour
+        {
+            None,
+            Stackable,
+            Resettable
+        }
+        public Character Sender { get; private set; }
+        public Character Reciever { get; private set; }
+
+        List<StatusEffectStack> _stacks;
 
         public void Initialize(Character sender)
         {
             Sender = sender;
+            _stacks = new List<StatusEffectStack>();
             DamageInfo = new DamageInfo(_damageValues);
             DamageInfo = DamageHandler.CalculateDamage(DamageInfo, sender.Stats);
         }
 
-        public void ApplyEffect(Character reciever)
+        public void SetReceiver(Character reciever) => Reciever = reciever;
+
+        public void ApplyEffect()
         {
-            Reciever = reciever;
-            Reciever.StatusEffect.AddEffect(this);
-            foreach (EffectBehaviour effect in Behaviours)
-            {
+            if (_stacks.Count == 0)
+                AddStack();
+            else {
+                switch (_stackingBehaviour) {
+                    case StatusBehaviour.None:
+                        break;
+                    case StatusBehaviour.Stackable:
+                        if (_stacks.Count - 1 < maxStacks) {
+                            AddStack();
+                        }
+                        break;
+                    case StatusBehaviour.Resettable:
+                        _stacks[0].ResetTimer();
+                        break;
+                }
+            }
+        }
+
+        public void AddStack()
+        {
+            _stacks.Add(new StatusEffectStack(this, lifeTime, tickSpeed));
+            foreach (EffectBehaviour effect in _behaviours) {
                 effect.OnStart(this);
             }
         }
 
-        public void Multiple()
+        public void RemoveStack(StatusEffectStack stack)
         {
-            switch (statusBehaviour)
-            {
-                case StatusBehaviour.None:
-                    break;
-                case StatusBehaviour.Stackable:
-                    if (_currentStacks < MaxStacks)
-                    {
-                        _currentStacks++;
-                    }
-                    break;
-                case StatusBehaviour.Resetable:
-                    _timer = 0; _nextTickTime = 0;
-                    break;
-            }
-        }
-
-        public void EndEffect()
-        {
-            _timer = 0;
-            _nextTickTime = 0;
-            foreach (EffectBehaviour effect in Behaviours)
-            {
+            _stacks.Remove(stack);
+            foreach (EffectBehaviour effect in _behaviours) {
                 effect.OnExit(this);
             }
-            Reciever.StatusEffect.RemoveEffect(this);
+            if(_stacks.Count == 0) {
+                Reciever.StatusEffect.RemoveEffect(this);
+            }
+        }
+
+        public void DoEffect()
+        {
+            foreach (EffectBehaviour behaviour in _behaviours) {
+                behaviour.DoEffect(this);
+            }
         }
 
 
-        public void UpdateStatus(float dt)
+        public void Update(float dt)
         {
-            _timer += dt;
-            if (_timer >= Lifetime) { EndEffect(); }
+            for (int i = 0; i < _stacks.Count; i++) {
+                _stacks[i].Update(dt);
+            }
+        }
 
-            if (_timer > _nextTickTime)
+
+        public class StatusEffectStack
+        {
+            readonly StatusEffect _owner;
+            float _tickSpeed;
+            float _nextTick;
+            float _lifeTime;
+            float _currentLifeTime;
+
+            public StatusEffectStack(StatusEffect owner, float lifeTime, float tickSpeed)
             {
-                _nextTickTime += TickSpeed;
-                for (int i = 0; i < _currentStacks; i++)
-                {
-                    foreach (EffectBehaviour behaviour in Behaviours)
-                    {
-                        behaviour.DoEffect(this);
-                    }
+                _owner = owner;
+                _lifeTime = lifeTime;
+                ResetTimer();
+            }
+
+            public void ResetTimer()
+            {
+                _currentLifeTime = _lifeTime;
+                _nextTick = 0;
+            }
+
+            public void Update(float dt)
+            {
+                _currentLifeTime -= dt;
+                if(_currentLifeTime <= 0) {
+                    _owner.RemoveStack(this);
+                }
+                if(_currentLifeTime >= _nextTick) {
+                    _owner.DoEffect();
+                    _nextTick = _currentLifeTime + _tickSpeed;
                 }
             }
         }
-    }
-
-    public enum StatusBehaviour
-    {
-        None,
-        Stackable,
-        Resetable
     }
 }
