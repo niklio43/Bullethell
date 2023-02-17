8b49098f13c79fb656aafe0ab6125ee2586f387a
EricLocher <eriloc1129@skola.goteborg.se>
2023-02-17 15:22:38 +0100

    DamageZone + Airborne projektile draft

---
+++ Assets/Scripts/Other/DamageZone/Scripts/DamageZoneManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BulletHell;
+
+public class DamageZoneManager : Singleton<DamageZoneManager>
+{
+    static ObjectPool<DamageZone> _zonePool;
+    static DamageZone _zone;
+
+    protected override void OnAwake()
+    {
+        _zone = Resources.Load<DamageZone>("DamageZone");
+        _zonePool = new ObjectPool<DamageZone>(Create, 100, "DamageZonePool", transform);
+    }
+
+    DamageZone Create()
+    {
+        DamageZone zone = Instantiate(_zone);
+        zone.Initialize(_zonePool);
+        return zone;
+    }
+
+    public static DamageZone PlaceZone(Vector2 position)
+    {
+        DamageZone zone = _zonePool.Get();
+        zone.gameObject.SetActive(true);
+        zone.transform.position = position;
+        return zone;
+    }
+
+}
