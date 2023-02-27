a98527b23eb305862ed0392d2b9eb757441da4d9
Niklas Jörgensen <niklio15csgo@hotmail.com>
2023-02-27 13:49:43 +0100

    temporary blood system

--- Assets/ForgeUI.cs
+++ Assets/ForgeUI.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using BulletHell.Player;
 
 public class ForgeUI : Singleton<ForgeUI>
 {
     [SerializeField] Slider _progressBar;
-    [SerializeField] TextMeshProUGUI _bloodTxt;
+    [SerializeField] TextMeshProUGUI _currentHealth;
+    [SerializeField] PlayerController _playerController;
     [HideInInspector] public bool IsUpgrading = false;
 
     private void Update()
     {
+        _currentHealth.text = string.Concat("Blood: ", _playerController.Character.Stats["Hp"].Get());
+
         if(!IsUpgrading) { _progressBar.value = 0; return; }
         _progressBar.value += Time.deltaTime;
     }
