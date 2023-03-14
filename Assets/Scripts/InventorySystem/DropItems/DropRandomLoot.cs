3084b7aceb0429f8ba5c1ad7f106122a890bda40
Niklas Jörgensen <niklio15csgo@hotmail.com>
2023-03-14 11:34:34 +0100

    Item drop system reworked

--- Assets/Scripts/InventorySystem/DropItems/DropRandomLoot.cs
+++ Assets/Scripts/InventorySystem/DropItems/DropRandomLoot.cs
@@ -3,26 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using BulletHell.InventorySystem;
 
-/* How to use:
- * add this script to object that needs to drop random loot
- * make serialized list of itemdrop class like this ([SerializeField] List<ItemDrop> _dropTable = new List<ItemDrop>();)
- * choose items and drop chances in inspector (effectively making your own loot table)
- * getcomponent on this script and call DropItem method when desired, eg:
- * public void TakeDamage(float value)
- * {
- *  _stats.Health -= value;
- *  if(_stats.Health <= 0)
- *  {
- *      GetComponent<DropRandomLoot>().DropItem(_dropTable);
- *  }
- * }
- * obviously needs to be broken out in other methods like OnDeath() but this is only example.
- */
-
-public class DropRandomLoot : MonoBehaviour, IDropLoot
+public class DropRandomLoot : Singleton<DropRandomLoot>, IDropLoot
 {
-    public void DropItem(List<ItemDrop> dropTable)
+    [SerializeField] Material _droppedItemMaterial;
+
+    public void DropItem(List<ItemDrop> dropTable, Transform tf)
     {
+        if(dropTable == null) { return; }
         var item = SelectRandomItem(dropTable);
         if (item == null) return;
 
@@ -30,10 +17,11 @@ public class DropRandomLoot : MonoBehaviour, IDropLoot
         {
             GameObject droppedItem = new GameObject(item[i].DisplayName);
 
-            droppedItem.transform.position = new Vector3(transform.position.x + Random.Range(-1.5f, 1.5f), transform.position.y + Random.Range(-3f, 3f));
+            droppedItem.transform.position = new Vector3(tf.position.x + Random.Range(-1.5f, 1.5f), tf.position.y + Random.Range(-3f, 3f));
 
             SpriteRenderer sr = droppedItem.AddComponent<SpriteRenderer>();
             sr.sprite = item[i].Sprite;
+            sr.material = _droppedItemMaterial;
 
             DroppedItem di = droppedItem.AddComponent<DroppedItem>();
             di.ItemData = item[i];
